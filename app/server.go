8f67ca35d858010c898f774443681e87cff84240
Colin Ho <colin.ho99@gmail.com>
2022-12-18 18:44:00 +0800

    concurrent clients

--- app/server.go
+++ app/server.go
@@ -14,23 +14,31 @@ func main() {
 		os.Exit(1)
 	}
 
-	conn, err := l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+			os.Exit(1)
+		}
+
+		go handleConnection(conn)
 	}
+}
+
+func handleConnection(conn net.Conn) {
 	defer conn.Close()
 
 	for {
 		buf := make([]byte, 1024)
+
+		_, err := conn.Read(buf)
 		if err == io.EOF {
 			break
 		}
-		if _, err := conn.Read(buf); err != nil {
+		if err != nil {
 			fmt.Println("error reading from client: ", err.Error())
 			os.Exit(1)
 		}
 		conn.Write([]byte("+PONG\r\n"))
 	}
-
 }
