2d0deb2c921356f66324b456777198b1a7dfe334
Colin Ho <colin.ho99@gmail.com>
2022-12-19 10:26:25 +0800

    storage

--- app/server.go
+++ app/server.go
@@ -16,6 +16,8 @@ func main() {
 		os.Exit(1)
 	}
 
+	storage := NewStorage()
+
 	for {
 		conn, err := l.Accept()
 		if err != nil {
@@ -23,11 +25,11 @@ func main() {
 			os.Exit(1)
 		}
 
-		go handleConnection(conn)
+		go handleConnection(conn, storage)
 	}
 }
 
-func handleConnection(conn net.Conn) {
+func handleConnection(conn net.Conn, storage *Storage) {
 	defer conn.Close()
 
 	for {
@@ -48,6 +50,11 @@ func handleConnection(conn net.Conn) {
 			conn.Write([]byte("+PONG\r\n"))
 		case "echo":
 			conn.Write([]byte(fmt.Sprintf("$%d\r\n%s\r\n", len(args[0].String()), args[0].String())))
+		case "set":
+			storage.Set(args[0].String(), args[1].String())
+			conn.Write([]byte("+OK\r\n"))
+		case "get":
+			conn.Write([]byte(fmt.Sprintf("+%s\r\n", storage.Get(args[0].String()))))
 		default:
 			conn.Write([]byte("-ERR unknown command '" + command + "'\r\n"))
 		}
